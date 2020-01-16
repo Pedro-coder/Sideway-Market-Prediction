c9e03806c208c00a6a7f2a64a62b5bc66ef4cec6
Pedro <conda0905@gmail.com>
2020-01-17 00:54:56 +0800

    Modified..

--- stock_predict.py
+++ stock_predict.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 data_source = 'alphavantage' # alphavantage or kaggle
 
-if data_source == 'alphavantage':
+if data_source == 'Kaggle':
     # ====================== Loading Data from Alpha Vantage ==================================
 
     api_key = '8AIKEJC5LK2RQVS2'
@@ -55,7 +55,7 @@ else:
     # ====================== Loading Data from Kaggle ==================================
     # You will be using HP's data. Feel free to experiment with other data.
     # But while doing so, be careful to have a large enough dataset and also pay attention to the data normalization
-    # df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
+    df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
     print('Loaded data from the Kaggle repository')
 
 # Sort DataFrame by date
@@ -74,61 +74,7 @@ plt.ylabel('Mid Price',fontsize=18)
 plt.show()
 
 
-
 # First calculate the mid prices from the highest and lowest
 high_prices = df.loc[:,'High'].as_matrix()
 low_prices = df.loc[:,'Low'].as_matrix()
 mid_prices = (high_prices+low_prices)/2.0
-
-
-split_point = 2000
-train_data = mid_prices[:split_point]
-test_data = mid_prices[split_point:]
-
-
-
-
-# Scale the data to be between 0 and 1
-# When scaling remember! You normalize both test and train data with respect to training data
-# Because you are not supposed to have access to test data
-
-scaler = MinMaxScaler()
-
-train_data = train_data.reshape(-1,1)
-test_data = test_data.reshape(-1,1)
-
-# Train the Scaler with training data and smooth data
-# print(train_data.size)
-smoothing_window_size = 100
-for di in range(0,train_data.size,smoothing_window_size):
-    scaler.fit(train_data[di:di+smoothing_window_size,:])
-    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
-
-# You normalize the last bit of remaining data
-# scaler.fit(train_data[di+smoothing_window_size:,:])
-# train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
-
-# Reshape both train and test data
-train_data = train_data.reshape(-1)
-
-# Normalize test data
-# scaler.fit(test_data[0:test_data.size,:])
-test_data = scaler.transform(test_data).reshape(-1)
-
-
-# Now perform exponential moving average smoothing
-# So the data will have a smoother curve than the original ragged data
-EMA = 0.0
-gamma = 0.1
-for ti in range(split_point):
-	EMA = gamma*train_data[ti] + (1-gamma)*EMA
-	train_data[ti] = EMA
-
-
-
-
-
-
-
-
-
