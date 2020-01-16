507ec581d4ecdd8c3dfc3d06fd652677cb03ae1c
Pedro <conda0905@gmail.com>
2020-01-16 20:51:26 +0800

    Error correction.

--- stock_predict.py
+++ stock_predict.py
@@ -80,31 +80,39 @@ high_prices = df.loc[:,'High'].as_matrix()
 low_prices = df.loc[:,'Low'].as_matrix()
 mid_prices = (high_prices+low_prices)/2.0
 
-train_data = mid_prices[:11000]
-test_data = mid_prices[11000:]
+
+split_point = 2000
+train_data = mid_prices[:split_point]
+test_data = mid_prices[split_point:]
+
+
 
 
 # Scale the data to be between 0 and 1
 # When scaling remember! You normalize both test and train data with respect to training data
 # Because you are not supposed to have access to test data
+
 scaler = MinMaxScaler()
+
 train_data = train_data.reshape(-1,1)
 test_data = test_data.reshape(-1,1)
 
 # Train the Scaler with training data and smooth data
-smoothing_window_size = 2500
-for di in range(0,10000,smoothing_window_size):
+# print(train_data.size)
+smoothing_window_size = 100
+for di in range(0,train_data.size,smoothing_window_size):
     scaler.fit(train_data[di:di+smoothing_window_size,:])
     train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
 
 # You normalize the last bit of remaining data
-scaler.fit(train_data[di+smoothing_window_size:,:])
-train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+# scaler.fit(train_data[di+smoothing_window_size:,:])
+# train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
 
 # Reshape both train and test data
 train_data = train_data.reshape(-1)
 
 # Normalize test data
+# scaler.fit(test_data[0:test_data.size,:])
 test_data = scaler.transform(test_data).reshape(-1)
 
 
@@ -112,7 +120,7 @@ test_data = scaler.transform(test_data).reshape(-1)
 # So the data will have a smoother curve than the original ragged data
 EMA = 0.0
 gamma = 0.1
-for ti in range(11000):
+for ti in range(split_point):
 	EMA = gamma*train_data[ti] + (1-gamma)*EMA
 	train_data[ti] = EMA
 
