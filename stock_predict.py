b9bdfbf9f388769e4bc8e1415e39f708060339e6
Pedro <conda0905@gmail.com>
2020-01-16 18:23:13 +0800

    error occured...

--- stock_predict.py
+++ stock_predict.py
@@ -62,8 +62,8 @@ else:
 df = df.sort_values('Date')
 
 # Double check the result
-df.head()
-df.tail()
+print(df.head())
+print(df.tail())
 
 
 plt.figure(figsize = (18,9))
@@ -90,6 +90,34 @@ test_data = mid_prices[11000:]
 scaler = MinMaxScaler()
 train_data = train_data.reshape(-1,1)
 test_data = test_data.reshape(-1,1)
+
+# Train the Scaler with training data and smooth data
+smoothing_window_size = 2500
+for di in range(0,10000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+# You normalize the last bit of remaining data
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+# Reshape both train and test data
+train_data = train_data.reshape(-1)
+
+# Normalize test data
+test_data = scaler.transform(test_data).reshape(-1)
+
+
+# Now perform exponential moving average smoothing
+# So the data will have a smoother curve than the original ragged data
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+	EMA = gamma*train_data[ti] + (1-gamma)*EMA
+	train_data[ti] = EMA
+
+
+
 
 
 
